6d126292763c890817a8bc1c6d1b95f692a149bf
Delide11 <tendy.jezy@gmail.com>
2022-10-13 05:59:25 -0700

    double quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,6 @@
 /* c program that prints out output*/
 int main(void)
 {	/* this will print out the result*/
-	printf("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Kopar, 2015-10-19 \n");
 	return 1;
 }
