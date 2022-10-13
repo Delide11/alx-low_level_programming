4719841360694bf8a58001ceba887ed76b156e0c
Delide11 <tendy.jezy@gmail.com>
2022-10-13 03:46:51 -0700

    size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("Size of a char: %zu bytes \n", sizeof(char));
+	printf("Size of an int: %zu bytes \n", sizeof(int));
+	printf("Size of a long int: %zu bytes \n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s) \n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s) \n",sizeof(float));
+	return 0;
+}
+
