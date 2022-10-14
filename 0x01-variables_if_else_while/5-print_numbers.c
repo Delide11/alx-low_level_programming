a7198eb1296301152a8d47d43817154a6a6cedfe
Delide11 <tendy.jezy@gmail.com>
2022-10-14 07:27:26 -0700

    printing numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/*all header files goes here*/
+/**
+ * main - entry point
+ * Description: 'the program's description'
+ * Return: Always (0) Success*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+		return (0);
+}
