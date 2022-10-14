21dbe3ebac87afafb51aefce7d562e06ccc29132
Delide11 <tendy.jezy@gmail.com>
2022-10-14 06:58:58 -0700

    not print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/*all header files goes here*/
+/**
+ * main - entry point
+ * Description: 'the program's description'
+ * Return: Always (0) Success*
+*/
+
+int main(void)
+{
+	char i;
+	char letter;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		letter = i;
+		if (letter != 'q' || letter != 'e')
+		putchar(i);
+	}
+		putchar('\n');
+		return (0);
+}
