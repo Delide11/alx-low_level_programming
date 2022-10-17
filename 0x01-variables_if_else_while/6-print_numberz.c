d9a5b6ed6777a24d3804930ed49148b2ba723aed
Delide11 <tendy.jezy@gmail.com>
2022-10-17 07:47:00 -0700

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,10 @@
 int main(void)
 {
 	int i;
-	int digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 		for (i = 0; i < 10; i++)
 		{
-			putchar(digits[i]);
+			putchar(i + '0');
 		}
 	putchar('\n');
 		return (0);
