0103462768416b27eaf5c3361c7bb0ab929cf081
Delide11 <tendy.jezy@gmail.com>
2022-10-17 05:32:41 -0700

    base16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/*all header files goes here*/
+/**
+ * main - entry point
+ * Description: 'the program's description'
+ * Return: Always (0) Success*
+*/
+
+int main(void)
+{
+	char y;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	for (y = 'A'; y <= 'F'; y++)
+	{
+		putchar(y);
+	}
+		putchar('\n');
+		return (0);
+}
