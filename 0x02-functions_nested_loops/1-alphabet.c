f989e2eba1f60ac44310c36484d0df24b1aa3a1d
Delide11 <tendy.jezy@gmail.com>
2022-10-19 04:47:51 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *  main -entry point
+ *
+ *  Description: 'the programs description'
+ *
+ *  Return: Alwas 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
+
+int main(void)
+{
+       print_alphabet();       
+	return (0);
+}
